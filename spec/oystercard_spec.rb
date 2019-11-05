3ba13685e363a59e4d672b04c12cb4d60400e63b
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-05 17:03:42 +0000

    Written failing test that checks that a new card has a balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,9 @@
+require 'oystercard'
+
 describe Oystercard do
+  it {is_expected.to be_instance_of(described_class)}
+
+  describe '#balance' do
+    it {is_expected.to respond_to(:balance)}
+  end
 end
