af5153f997c3445b7605c60da37453d9ddd9c042
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-05 17:24:27 +0000

    Refactored code by setting the balance in initialize using an instance variable

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,8 @@
 class Oystercard
+  def initialize(balance: 0)
+    @balance = balance
+  end
   def balance
+    @balance
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,5 +5,9 @@ describe Oystercard do
 
   describe '#balance' do
     it {is_expected.to respond_to(:balance)}
+
+    it 'returns balance of zero' do
+      expect(subject.balance).to eq 0
+    end
   end
 end
