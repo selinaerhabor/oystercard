e76143f35b9f21ecec073e819fe1eb00e7ea22d8
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-06 16:47:26 +0000

    Test if top up error is implemented

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,14 @@
 class Oystercard
+  MAX_BALANCE_LIMIT = 90
   attr_reader :balance
   def initialize(balance: 0)
     @balance = balance
   end
   def top_up(amount)
+    if balance + amount > MAX_BALANCE_LIMIT
+      raise "Invalid amount, maximum balance limit is #{MAX_BALANCE_LIMIT}"
+    end
+
     @balance += amount
   end
 end
