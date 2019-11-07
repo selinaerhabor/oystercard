90ee19b1a6760416ffa1e3f4cc38c70303668aca
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-07 14:18:35 +0000

    Added test for Journey instance variable. Test currently failing

---
+++ spec/journey_spec.rb
@@ -0,0 +1,8 @@
+require './lib/journey'
+
+describe Journey do
+  subject(:journey) {Journey.new}
+
+  it {is_expected.to be_instance_of(Journey)}
+  it {is_expected.to respond_to(:in_journey)}
+end
